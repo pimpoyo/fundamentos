51f4cd45108260a542e44d6b3fd8a4cee6a3e80e
Alumnos <alumnos@iMac-lab07.local>
2015-05-14 11:57:38 +0200

    Definicion de variables en php

---
+++ primero.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Mi primer programa en PHP</title>
+</head>
+<body>
+	<?php
+		print "<p><strong> Ana</strong></p>\n";
+		print "<p><strong> Deporte</strong></p>\n";
+		print "<p><strong> Artesan&iacutea</strong></p>\n";
+		print "<p><strong> Paisajismo acu&aacutetico</strong></p>\n";
+		
+		$edad = 25;
+		print "<p>La edad es $edad</p>\n"; //muestra "La edad es 25" y avanza una linea
+		print "<p>Mi libro favorito es \"El nombre del viento\"</p>\n";
+	?>
+</body>
+</html>
