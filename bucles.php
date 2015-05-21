93fad047369e1a2b96e8327346edd67f33c5b491
Alumnos <alumnos@iMac-lab07.local>
2015-05-21 11:17:44 +0200

    Utilización bucle for

---
+++ bucles.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>Bucles</title>
+</head>
+<body>
+	<?php
+		echo "<ul>\n";
+		for ($n=1; $n<=5; $n++) {
+			echo "<li>Elemento" . $n . "</li>\n";
+		}
+		echo "</ul>\n";
+	?>
+</body>
+</html>
