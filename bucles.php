c0a6ef30cf35e98d169b595ec0df5e6b29fa03f8
Alumnos <alumnos@iMac-lab07.local>
2015-05-21 11:52:24 +0200

    For con condiciones combinadas dentro

--- bucles.php
+++ bucles.php
@@ -5,8 +5,10 @@
 <body>
 	<?php
 		echo "<ul>\n";
-		for ($n=1; $n<=5; $n++) {
-			echo "<li>Elemento" . $n . "</li>\n";
+		for ($n=1; $n<=150; $n=$n+4) {
+			if ($n < 20 || $n > 50) {
+				echo "<li>Elemento" . $n . "</li>\n";
+			}
 		}
 		echo "</ul>\n";
 	?>
