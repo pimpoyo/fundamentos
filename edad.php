205938002e4b80ec31091ffd4c305009bf979314
Alumnos <alumnos@iMac-lab07.local>
2015-05-21 10:26:47 +0200

    Poniendo sexo a la edad

--- edad.php
+++ edad.php
@@ -4,18 +4,23 @@
 </head>
 <body>
 	<?php
-		$edad = 25;
-		
+		$edad = 23;
+		$sexo = "mujer";
+
+		if ($sexo == "mujer") {
+			$edad = $edad - 5;
+		}
+
 		if ($edad < 5) {
-			echo "<p>Eres un beb&eacute</p>";
+			echo "<p>Eres un beb&eacute, tienes " . $edad . " a&#241os</p>";
 		} elseif ($edad < 10) {
-			echo "<p>Eres un nene</p>";
+			echo "<p>Eres un nene, tienes " . $edad . " a&#241os</p>";
 		} elseif ($edad < 15) {
-			echo "<p>Eres un preadolescente</p>";
+			echo "<p>Eres un preadolescente, tienes " . $edad . " a&#241os</p>";
 		} elseif ($edad < 20) {
-			echo "<p>Eres un adolescente</p>";
+			echo "<p>Eres un adolescente, tienes " . $edad . " a&#241os</p>";
 		} else {
-			echo "<p>Pa'ti es la vida</p>";
+			echo "<p>Pa'ti es la vida, tienes " . $edad . " a&#241os</p>";
 		}
 	?>
 </body>
