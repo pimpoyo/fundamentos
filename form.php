5cf6560f5a1f9016af454f7c6b1e9c2d8f76644e
Alumnos <alumnos@iMac-lab07.local>
2015-05-21 12:29:31 +0200

    Fix echo sentences

--- form.php
+++ form.php
@@ -3,6 +3,9 @@
 	<?php
 		$edad = $_REQUEST['edad'];
 		$sexo = $_REQUEST['sexo'];
+		
+		//var_dump($_REQUEST);
+		
 		echo '<p>Tu edad es ' . $edad . '</p>';
 		echo '<p>Eres ' . $sexo . '</p>';
 		
@@ -13,30 +16,30 @@
 
 		if ($edad < 5) {
 			if ($sexo == "mujer") {
-				echo "<p>Eres una beb&eacute, tienes " . $edad . " a&#241os</p>";
+				echo '<p>Eres una beb&eacute, tienes ' . $edad . ' a&#241os</p>';
 			} else {
-				echo "<p>Eres un beb&eacute, tienes " . $edad . " a&#241os</p>";
+				echo '<p>Eres un beb&eacute, tienes ' . $edad . ' a&#241os</p>';
 			}
 		} elseif ($edad < 10) {
 			if ($sexo == "mujer") {
-				echo "<p>Eres una nena, tienes " . $edad . " a&#241os</p>";
+				echo '<p>Eres una nena, tienes ' . $edad . ' a&#241os</p>';
 			} else {
-				echo "<p>Eres un nene, tienes " . $edad . " a&#241os</p>";
+				echo '<p>Eres un nene, tienes ' . $edad . ' a&#241os</p>';
 			}
 		} elseif ($edad < 15) {
 			if ($sexo == "mujer") {
-				echo "<p>Eres una preadolescente, tienes " . $edad . " a&#241os</p>";
+				echo '<p>Eres una preadolescente, tienes ' . $edad . ' a&#241os</p>';
 			} else {
-				echo "<p>Eres un preadolescente, tienes " . $edad . " a&#241os</p>";
+				echo '<p>Eres un preadolescente, tienes ' . $edad . ' a&#241os</p>';
 			}
 		} elseif ($edad < 20) {
-			if ($sexo == "mujer") {
-				echo "<p>Eres una adolescente, tienes " . $edad . " a&#241os</p>";
+			if ($sexo == 'mujer') {
+				echo '<p>Eres una adolescente, tienes ' . $edad . ' a&#241os</p>';
 			} else {
-				echo "<p>Eres un adolescente, tienes " . $edad . " a&#241os</p>";
+				echo '<p>Eres un adolescente, tienes ' . $edad . ' a&#241os</p>';
 			}
 		} else {
-			echo "<p>Pa'ti es la vida, tienes " . $edad . " a&#241os</p>";
+			echo '<p>Pa\'ti es la vida, tienes ' . $edad . ' a&#241os</p>';
 		}
 	?>
 </body>
