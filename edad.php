7dfa878065edc7f1dba9731835c5af8dcb4ca94f
Alumnos <alumnos@iMac-lab07.local>
2015-05-21 10:45:25 +0200

    Con if anidados

--- edad.php
+++ edad.php
@@ -12,13 +12,29 @@
 		}
 
 		if ($edad < 5) {
-			echo "<p>Eres un beb&eacute, tienes " . $edad . " a&#241os</p>";
+			if ($sexo == "mujer") {
+				echo "<p>Eres una beb&eacute, tienes " . $edad . " a&#241os</p>";
+			} else {
+				echo "<p>Eres un beb&eacute, tienes " . $edad . " a&#241os</p>";
+			}
 		} elseif ($edad < 10) {
-			echo "<p>Eres un nene, tienes " . $edad . " a&#241os</p>";
+			if ($sexo == "mujer") {
+				echo "<p>Eres una nena, tienes " . $edad . " a&#241os</p>";
+			} else {
+				echo "<p>Eres un nene, tienes " . $edad . " a&#241os</p>";
+			}
 		} elseif ($edad < 15) {
-			echo "<p>Eres un preadolescente, tienes " . $edad . " a&#241os</p>";
+			if ($sexo == "mujer") {
+				echo "<p>Eres una preadolescente, tienes " . $edad . " a&#241os</p>";
+			} else {
+				echo "<p>Eres un preadolescente, tienes " . $edad . " a&#241os</p>";
+			}
 		} elseif ($edad < 20) {
-			echo "<p>Eres un adolescente, tienes " . $edad . " a&#241os</p>";
+			if ($sexo == "mujer") {
+				echo "<p>Eres una adolescente, tienes " . $edad . " a&#241os</p>";
+			} else {
+				echo "<p>Eres un adolescente, tienes " . $edad . " a&#241os</p>";
+			}
 		} else {
 			echo "<p>Pa'ti es la vida, tienes " . $edad . " a&#241os</p>";
 		}
