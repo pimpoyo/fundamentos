a7ee97b837d7845fd03d3fce6865ece796a9848f
Alumnos <alumnos@iMac-lab07.local>
2015-05-21 13:33:01 +0200

    Formulario con comprobaciones

--- form.php
+++ form.php
@@ -1,45 +1,69 @@
 <html>
 <body>
 	<?php
-		$edad = $_REQUEST['edad'];
-		$sexo = $_REQUEST['sexo'];
+		$flag = true;
 		
-		//var_dump($_REQUEST);
-		
-		echo '<p>Tu edad es ' . $edad . '</p>';
-		echo '<p>Eres ' . $sexo . '</p>';
-		
-
-		if ($sexo == "mujer") {
-			$edad = $edad - 5;
-		}
-
-		if ($edad < 5) {
-			if ($sexo == "mujer") {
-				echo '<p>Eres una beb&eacute, tienes ' . $edad . ' a&#241os</p>';
-			} else {
-				echo '<p>Eres un beb&eacute, tienes ' . $edad . ' a&#241os</p>';
-			}
-		} elseif ($edad < 10) {
-			if ($sexo == "mujer") {
-				echo '<p>Eres una nena, tienes ' . $edad . ' a&#241os</p>';
-			} else {
-				echo '<p>Eres un nene, tienes ' . $edad . ' a&#241os</p>';
-			}
-		} elseif ($edad < 15) {
-			if ($sexo == "mujer") {
-				echo '<p>Eres una preadolescente, tienes ' . $edad . ' a&#241os</p>';
-			} else {
-				echo '<p>Eres un preadolescente, tienes ' . $edad . ' a&#241os</p>';
-			}
-		} elseif ($edad < 20) {
-			if ($sexo == 'mujer') {
-				echo '<p>Eres una adolescente, tienes ' . $edad . ' a&#241os</p>';
-			} else {
-				echo '<p>Eres un adolescente, tienes ' . $edad . ' a&#241os</p>';
+		if($_REQUEST['edad'] != "") {
+			$edad = (int)$_REQUEST['edad'];
+			if ($_REQUEST['edad'] != "$edad") {
+				echo '<p>Edad no v&aacutelida<p>';
+				$flag = false;
 			}
 		} else {
-			echo '<p>Pa\'ti es la vida, tienes ' . $edad . ' a&#241os</p>';
+			echo '<p>Edad vac&iacutea<p>';
+			$flag = false;
+		}
+		
+		if($_REQUEST['sexo'] != "") {
+			$sexo = strtoupper($_REQUEST['sexo']);
+			if ($sexo != "HOMBRE" && $sexo!="MUJER") {
+				echo '<p>Sexo no admitido<p>';
+				$flag = false;
+			}
+		} else {
+			echo '<p>Sexo vac&iacutea<p>';
+			$flag = false;
+		}
+	
+
+		if ($flag == true) {
+			echo '<p>Tu edad es ' . $edad . '</p>';
+			echo '<p>Eres ' . $_REQUEST['sexo'] . '</p>';
+			
+	
+			if ($sexo == "MUJER") {
+				$edad = $edad - 5;
+			}
+	
+			if ($edad < 5) {
+				if ($sexo == "MUJER") {
+					echo '<p>Eres una beb&eacute, tienes ' . $edad . ' a&#241os</p>';
+				} else {
+					echo '<p>Eres un beb&eacute, tienes ' . $edad . ' a&#241os</p>';
+				}
+			} elseif ($edad < 10) {
+				if ($sexo == "MUJER") {
+					echo '<p>Eres una nena, tienes ' . $edad . ' a&#241os</p>';
+				} else {
+					echo '<p>Eres un nene, tienes ' . $edad . ' a&#241os</p>';
+				}
+			} elseif ($edad < 15) {
+				if ($sexo == "MUJER") {
+					echo '<p>Eres una preadolescente, tienes ' . $edad . ' a&#241os</p>';
+				} else {
+					echo '<p>Eres un preadolescente, tienes ' . $edad . ' a&#241os</p>';
+				}
+			} elseif ($edad < 20) {
+				if ($sexo == 'MUJER') {
+					echo '<p>Eres una adolescente, tienes ' . $edad . ' a&#241os</p>';
+				} else {
+					echo '<p>Eres un adolescente, tienes ' . $edad . ' a&#241os</p>';
+				}
+			} else {
+				echo '<p>Pa\'ti es la vida, tienes ' . $edad . ' a&#241os</p>';
+			}
+		} else {
+			echo 'ERROR: Campos incorrectos en el formulario.';
 		}
 	?>
 </body>
