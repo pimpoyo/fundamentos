d9cfc8e6815bf677e2b4d0d749659417222d9bfd
Juan Arias <pimpoyolega@gmail.com>
2015-06-18 08:13:08 +0200

    Refactorización de procesarpedido.php, creación de tabla hash para producto

--- Grupo1/procesapedido.php
+++ Grupo1/procesapedido.php
@@ -1,88 +1,75 @@
 <?php
-  /* Refactorizar */
-  $product_price[1] = $_REQUEST['product_price1'];
-  $product_name[1] = $_REQUEST['product_name1'];
-  $amount[1] = $_REQUEST['amount1'];
-  $subtotal[1] = $amount[1] * $product_price[1];
-  
-  $product_price[2] = $_REQUEST['product_price2'];
-  $product_name[2] = $_REQUEST['product_name2'];
-  $amount[2] = $_REQUEST['amount2'];
-  $subtotal[2] = $amount[2] * $product_price[2];
 
-  $product_price[3] = $_REQUEST['product_price3'];
-  $product_name[3] = $_REQUEST['product_name3'];
-  $amount[3] = $_REQUEST['amount3'];
-  $subtotal[3] = $amount[3] * $product_price[3];
+// Número de productos totales que hay en el html
+$products_number = 6;
 
-  $product_price[4] = $_REQUEST['product_price4'];
-  $product_name[4] = $_REQUEST['product_name4'];
-  $amount[4] = $_REQUEST['amount4'];
-  $subtotal[4] = $amount[4] * $product_price[4];
+for ($i = 1; $i<=$products_number; $i++) {
+	// Se genera un array de productos y cada producto a su vez es un array asociativo
+	//Se construye el array asociativo con los datos cuya cantidad es distinta de 0
+	if ($_REQUEST["amount$i"] != 0) {
+		$products[$i]['price'] = $_REQUEST["product_price$i"];
+		$products[$i]['name'] = $_REQUEST["product_name$i"];
+		$products[$i]['amount'] = $_REQUEST["amount$i"];
+		$products[$i]['subtotal'] = $products[$i]['amount'] * $products[$i]['price'];
+	}
+}
 
-  $product_price[5] = $_REQUEST['product_price5'];
-  $product_name[5] = $_REQUEST['product_name5'];
-  $amount[5] = $_REQUEST['amount5'];
-  $subtotal[5] = $amount[5] * $product_price[5];
+$subtotal_no_iva = 0;
 
-  $product_price[6] = $_REQUEST['product_price6'];
-  $product_name[6] = $_REQUEST['product_name6'];
-  $amount[6] = $_REQUEST['amount6'];
-  $subtotal[6] = $amount[6] * $product_price[6];
+// Se suman los subtotales de los productos
+foreach ($products as $product) {
+	$subtotal_no_iva += $product['subtotal'];
+}
 
-  $subtotal_no_iva = 0;
-  foreach ($subtotal as $sub) {
-    $subtotal_no_iva += $sub;
-  }
-  
-  $subtotal_with_iva = $subtotal_no_iva * 1.21;
+// Se calcula el precio total con iva
+$subtotal_with_iva = $subtotal_no_iva * 1.21;
 ?>
 
 <html>
 <head>
 	<title></title>
 	<meta charset="utf-8" /> 
-  <link rel="stylesheet" href="form.css">
+	<link rel="stylesheet" href="form.css">
 </head>
 <body>
-      
-    <div class="form">
-    <div class="logo"><a href="index.html"><img class="logo" src="img/logo.png" alt="logo"/></a></div>
-        <a href="index.html"><p class="form-header">InnoTech</p></a>
-           
-    <p class="form-step-on">Tu compra</p>
-    <div class="form2">
-      <div class="form-container">
-        
-          <table>
+
+	<div class="form">
+	<div class="logo"><a href="index.html"><img class="logo" src="img/logo.png" alt="logo"/></a></div>
+		<a href="index.html"><p class="form-header">InnoTech</p></a>
+
+	<p class="form-step-on">Tu compra</p>
+	<div class="form2">
+	  <div class="form-container">
+
+		  <table>
 <?php
-        $i = 1;
-        foreach ($subtotal as $sub) {
-          echo '<tr>';
-            echo '<td>' . $product_name[$i] . '</td>'; 
-            echo '<td>' . $product_price[$i] . '</td>'; 
-            echo '<td>' . $amount[$i] . '</td>'; 
-            echo '<td>' . $sub . '</td>'; 
-          echo '</tr>'; 
-          $i++;
-        }
+
+// Se construyen las filas de la tabla que se va a mostrar como resultado
+foreach ($products as $product) {
+	echo '<tr>';
+	echo '<td>' . $product['name'] . '</td>'; 
+	echo '<td>' . $product['price'] . '</td>'; 
+	echo '<td>' . $product['amount'] . '</td>'; 
+	echo '<td>' . $product['subtotal'] . '</td>'; 
+	echo '</tr>'; 
+}
 ?> 
-        </table>
+		</table>
 <?
-        echo '<div>Precio total sin IVA: ' . $subtotal_no_iva . '</div>';
-        echo '<div>Precio total con IVA: ' . $subtotal_with_iva . '</div>';
+echo '<div>Precio total sin IVA: ' . $subtotal_no_iva . '</div>';
+echo '<div>Precio total con IVA: ' . $subtotal_with_iva . '</div>';
 ?>
-      </div>
-    </div>
+	  </div>
+	</div>
   </div>
-    
-    <footer class="form">
-            <div class="footer">
-                <img src="img/f.png" alt="facebook" />
-                <img src="img/t.png" alt="twitter" />
-                <img src="img/p.png" alt="pinterest" />
-                <img src="img/youTube.png" alt="youtube" />
-                <img src="img/in.png" alt="linkedin" />
-            </div>
-        </footer>
+
+	<footer class="form">
+			<div class="footer">
+				<img src="img/f.png" alt="facebook" />
+				<img src="img/t.png" alt="twitter" />
+				<img src="img/p.png" alt="pinterest" />
+				<img src="img/youTube.png" alt="youtube" />
+				<img src="img/in.png" alt="linkedin" />
+			</div>
+		</footer>
 </body>
